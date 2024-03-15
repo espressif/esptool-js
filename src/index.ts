0a78d36e394f0aadf3e6e24bfb902638de48dddc
Brian Ignacio <brian.ignacio@espressif.com>
2024-03-15 12:22:09 +0800

    fix lint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 export { IEspLoaderTerminal, ESPLoader, FlashOptions, LoaderOptions } from "./esploader.js";
-export { classicReset, customReset, hardReset, usbJTAGSerialReset, validateCustomResetStringSequence } from "./reset.js";
+export {
+  classicReset,
+  customReset,
+  hardReset,
+  usbJTAGSerialReset,
+  validateCustomResetStringSequence,
+} from "./reset.js";
 export { ROM } from "./targets/rom.js";
 export { Transport, SerialOptions } from "./webserial.js";
